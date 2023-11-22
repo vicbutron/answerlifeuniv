654a24ddb2f5f44302f6e43de76b8b4b3bbf40c2
victorbutron <victoralejandrobutron@gmail.com>
2023-11-22 19:17:31 +0100

    Initial commit

---
+++ R/answerlifeuniv.R
@@ -0,0 +1,14 @@
+#' The Answer to the Ultimate Question of Life, the Universe, and Everything
+#'
+#' This is very self explanatory
+#'
+#' @param x Any input, really
+#'
+#' @return The answer to the ultimate question, which is 42.
+#' @export
+#'
+#' @examples
+#' ultimate_answer(999999)
+ultimate_answer <- function(x) {
+  return(42)
+}
